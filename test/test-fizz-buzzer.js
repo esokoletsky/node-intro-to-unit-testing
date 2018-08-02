e7fb29c0b1fb6aeb57f06c5df33db782bd021cf0
Eugene S <esokoletsky@gmail.com>
2018-08-02 11:16:29 -0400

    added fizzBuzz test

---
+++ test/test-fizz-buzzer.js
@@ -0,0 +1,11 @@
+const expect = rquire('chai').expect;
+
+const fizzBuzzer = require('../test-fizz-buzzer');
+
+describe('fizzBuzzer', function(){
+	it('It should return a number', function(){
+		const normalCases = [
+			
+		]
+	})
+})
