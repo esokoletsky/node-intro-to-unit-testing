0e1b9dab004af951253595c3604131828874ffa7
esokoletsky <Esokoletsky@gmail.com>
2018-08-03 20:38:48 -0400

    test complete

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -1,11 +1,37 @@
-const expect = rquire('chai').expect;
+const expect = require('chai').expect;
 
-const fizzBuzzer = require('../test-fizz-buzzer');
+const fizzBuzzer = require('../fizzBuzzer');
 
 describe('fizzBuzzer', function(){
-	it('It should return a number', function(){
-		const normalCases = [
-			
-		]
-	})
-})
+
+	it('should return fizz-buzz for multiples of 15', function() {
+		[15, 30, 45].forEach(function(input) {
+			expect(fizzBuzzer(input)).to.equal('fizz-buzz');
+		});
+	});
+
+	it('should return fizz for multiples of 3', function(){
+		[3, 6 ,9, 12].forEach(function(input){
+			expect(fizzBuzzer(input)).to.equal('fizz');
+		});
+	});
+
+	it('should return buzz for multiples of 5', function(){
+		[5, 10, 15, 20].forEach(function(input){
+			expect(fizzBuzzer(input)).to.equal('buzz');
+		});
+	});
+
+	it('should return a number if not multiples of 3 or 5', function(){
+		[7, 11, 13, 16].forEach(function(input){
+			expect(fizzBuzzer(input)).to.equal(input);
+		});
+	});
+
+	it ('should equall a number', function(){
+		[true, false, 'cup'].forEach(function(input){
+			expect(fizzBuzzer(input)).to.throw(Error);
+		});
+	});
+
+});
